bfa0ac99c6295ece97e2eb0ad5917c702579182b
HadarSalame <hadarSalame99@gmail.com>
2022-03-02 20:49:16 +0200

    Update App.js
    imports

--- take_away/src/App.js
+++ take_away/src/App.js
@@ -2,7 +2,11 @@ import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './Components/Header/HeaderComponent'
-
+import Footer from './Components/Footer/FooterComponent'
+import Login from './Components/Claint/Login/LoginComponent'
+import SignUp from './Components/Claint/SignUp/SignUPComponent'
+import ProfessionalLogin from './Components/Professional/ProfessionalLogin/ProfessionalLoginComponent'
+import ProfessionalSignUp from './Components/Professional/ProfessionalSignUp/ProfessionalSignUpComponent'
 
 
 function App() {
