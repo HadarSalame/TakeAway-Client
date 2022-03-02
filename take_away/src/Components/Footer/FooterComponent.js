1aa96e4e006e4356f0e7afd394f59ebc9e229b27
HadarSalame <hadarSalame99@gmail.com>
2022-03-02 20:54:49 +0200

    add footer component

--- take_away/src/Components/Footer/FooterComponent.js
+++ take_away/src/Components/Footer/FooterComponent.js
@@ -1,1 +1,12 @@
 import React from 'react';
+import { Button, Navbar, Container, Nav } from 'react-bootstrap'
+import './FooterCSS.css'
+
+export default function Footer(props){
+
+    return(
+        <>
+        
+        </>
+    )
+}
