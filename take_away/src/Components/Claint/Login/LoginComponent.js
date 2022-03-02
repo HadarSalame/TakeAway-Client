fca7b24221a5393bd89b0f625e34b538703e954e
HadarSalame <hadarSalame99@gmail.com>
2022-03-02 20:52:12 +0200

    updeat claint
    add login and sign up components
    start them

--- take_away/src/Components/Claint/Login/LoginComponent.js
+++ take_away/src/Components/Claint/Login/LoginComponent.js
@@ -1,1 +1,15 @@
 import React from 'react';
+import './LoginCSS.css'
+import { Button } from 'react-bootstrap';
+
+export default function Login(props) {
+    return (
+        <>
+        <div>
+            {/* //התחברות למשתמשים עם שם סיסמא כפתור שיחזור סיסמה עי שליחת 
+            //קישור למייל 
+            // וכפתור התחברות */}
+        </div>
+        </>
+    )
+}
--- take_away/src/Components/Claint/SignUp/SignUPComponent.js
+++ take_away/src/Components/Claint/SignUp/SignUPComponent.js
@@ -1,1 +1,20 @@
 import React from 'react';
+import './SignUpCss.css'
+import { Button } from 'react-bootstrap';
+
+export default function SignUp(props) {
+    return (
+        <>
+            <div >
+                {/* for users */}
+                <div className='signUp' >
+                    {/* //הרשמה למשתמשים מכילה
+                    //שם, שם משפחה, נייד, מייל, סיסמה, אימות סיסמה
+                    //אישור תנאי האתר וכפתור הרשמה */}
+                    <h1>הרשמה למשתמשים</h1>
+                </div>
+
+            </div>
+        </>
+    )
+}
