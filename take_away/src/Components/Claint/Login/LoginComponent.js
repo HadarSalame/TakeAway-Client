6dc48c15bac9e69bc5669590ccfab81709bc301c
HadarSalame <hadarSalame99@gmail.com>
2022-03-08 22:13:53 +0200

    update login css

--- take_away/src/Components/Claint/Login/LoginComponent.js
+++ take_away/src/Components/Claint/Login/LoginComponent.js
@@ -5,18 +5,23 @@ import { Button } from 'react-bootstrap';
 export default function Login(props) {
     return (
         <>
-        <div>
+        <div >
             {/* //התחברות למשתמשים עם שם סיסמא כפתור שיחזור סיסמה עי שליחת 
             //קישור למייל 
             // וכפתור התחברות */}
             <br></br>
             <h1>התחברות</h1>
+            <br></br>
+            <br></br>
             <div className="border">
                 {/* <input type="email" value="LogInEmail" className="input">E-mail</input>
                 <br></br>
                 <input type="password" value="LogInPass">סיסמה</input>
                 <br></br>  */}
-               <Button value="logInBtn" className="btn">התחבר</Button>
+                <br></br>
+
+
+               <Button value="logInBtn" variant="outline" className="btn">התחברות</Button>
             </div>
         </div>
         </>
