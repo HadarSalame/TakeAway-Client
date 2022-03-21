ec75a3bcf7feacc129d809a11723d967dd8b335b
student <student@BA.local>
2022-03-21 11:37:46 +0200

    update

--- take_away/src/Components/Claint/Login/LoginComponent.js
+++ take_away/src/Components/Claint/Login/LoginComponent.js
@@ -13,9 +13,10 @@ export default function Login(props) {
                 <h1>התחברות</h1>
                 <br></br>
                 <br></br>
-                <div className="border" >
-                    {/* mail for login */}
-                    {/* <div style={{'marginBottom':'30px'}}>
+                <div className="border row" >
+                    <div className='col-xl-12 col-sm-12 col-xs-10'>
+                        {/* mail for login */}
+                        {/* <div style={{'marginBottom':'30px'}}>
                         <InputGroup className="mb-3">
                             <FormControl
                                 placeholder="E-mail"
@@ -27,7 +28,7 @@ export default function Login(props) {
                         </InputGroup>
                     </div>
                     {/* PassWord for login */}
-                    {/* <InputGroup className="mb-3">
+                        {/* <InputGroup className="mb-3">
 
                         <FormControl
                             placeholder="Password"
@@ -36,34 +37,35 @@ export default function Login(props) {
                             className='a'
                         />
                         <InputGroup.Text id="basic-addon1" >@</InputGroup.Text> */}
-                    {/* </InputGroup> */}
-                    <div>
-                        <FloatingLabel
-                            className="mb-3"
-                            style={{ 'direction': 'rtl' }}
-                            controlId="floatingInput"
-                            label="Email" >
+                        {/* </InputGroup> */}
+                        <div>
+                            <FloatingLabel
+                                className="mb-3"
+                                style={{ 'direction': 'rtl' }}
+                                controlId="floatingInput"
+                                label="Email" >
 
-                            <Form.Control
-                                type="email"
-                                placeholder="name@example.com" />
+                                <Form.Control
+                                    type="email"
+                                    placeholder="name@example.com" />
 
 
-                        </FloatingLabel>
+                            </FloatingLabel>
 
-                        <FloatingLabel
-                            controlId="floatingPassword"
-                            label="סיסמה">
+                            <FloatingLabel
+                                controlId="floatingPassword"
+                                label="סיסמה">
 
-                            <Form.Control
-                                type="password"
-                                placeholder="Password" />
+                                <Form.Control
+                                    type="password"
+                                    placeholder="Password" />
 
-                        </FloatingLabel>
+                            </FloatingLabel>
+                        </div>
+                        <h1 className='ForgetPassword'>שכחתי סיסמה</h1>
+
+                        <Button value="logInBtn" variant="outline" className="btn">התחבר/י</Button>
                     </div>
-                    <h1 className='ForgetPassword'>שכחתי סיסמה</h1>
-
-                    <Button value="logInBtn" variant="outline" className="btn">התחבר/י</Button>
                 </div>
             </div>
         </>
